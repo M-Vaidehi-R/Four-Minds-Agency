3d299d2c38a9e153cf2c95eb02f7dda227aaa229
Tallyta Triolo <tally.soares@gmail.com>
2022-09-11 03:44:07 -0400

    updating codes and fixing layout

--- js/main.js
+++ js/main.js
@@ -15,4 +15,23 @@ BMenu.addEventListener("click", ()=> {
     }
 })
 
+// Variable
+let overlayPanel = document.querySelector("#overlay-panel");
+let btnVideo = document.querySelector("#btn-video");
+let btnCloseOverlayPanel = document.querySelector("#overlay-close-btn");
+let agencyVideo = document.querySelector('video');
+
+function closeOverlayPanel() {
+    overlayPanel.style.display = "none";
+    agencyVideo.currentTime = 0;
+    agencyVideo.pause();
+}
+
+function openOverlayPanel() {
+    overlayPanel.style.display = "block";
+}
+
+btnVideo.addEventListener('click', openOverlayPanel);
+btnCloseOverlayPanel.addEventListener('click', closeOverlayPanel);
+
 })();
